24487cba2324f4e5ac87396042dd2a31a53a2387
TERAN <TERAN@TERAN>
2017-06-08 05:32:20 -0500

    nueva

---
+++ js/javascript.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function(){
+    $(".btn").click(function(){
+        $(this).button('loading');
+    });   
+});
+
+
+
+function direccion(){
+location.href='CentrosTuristicosCajamarca.html';
+}
+
+
+function direccion2(){
+location.href='https://www.w3schools.com';
+}
+function direccion3(){
+	location.href='https://www.javascript.com/';
+}
+function direccion4(){
+	location.href='https://www.microsoft.com/es-es';
+
+}
+function direccion5(){
+	location.href='https://www.unc.edu.pe/';
+}
+function direccion6(){
+	location.href='index.html';
+}
+function direccion7(){
+	location.href='historiaBa.html';
+}
+function direccion8(){
+	location.href='historiaVe.html'
+}
+
+function vermas() {
+    document.getElementById("mas").innerHTML = "Se encuentra a 2515 msnm. Además se encuentra circundado por el río Chonta, el cual se une al río Mashcon en la localidad de Huayrapongo y forman el río Cajamarquino. Éste a su vez es afluente del río Crisnejas (Tributario del Río Marañón).";
+}
+function vermas1(){
+	document.getElementById("mas1").innerHTML="Está situado cerca del poblado Otuzco en el distrito de Baños del Inca a 8 km al noroeste de la ciudad de Cajamarca. El sitio se localiza sobre roca volcánica. Tiene un área de 4 mil metros.";
+}
+
+function vermas2(){
+	document.getElementById("mas2").innerHTML="El sitio es especialmente conocido como yacimiento arqueológico, en el cual destacan las ruinas de un acueducto pre-incaico que posee unos 8 km de longitud. El acueducto juntaba agua de precipitaciones y la transportaba hacia la zona del océano Pacífico. Se cree que puede haber sido construido hacia el 1500 a. C. y antiguamente se pensaba que era la estructura existente más antigua de América del Sur. El nombre Cumbemayo puede tener sus raíces en la frase quechua, kumpi mayu, que significa “canal de agua bien construido”, o humpi mayo, que significa “río angosto.”";
+}
+
+
+function vermas3(){
+	document.getElementById("mas3").innerHTML="Este cerro de Santa Apolinia tiene una altura de 2,764 metros sobre el nivel del mar.Se ubica al lado Oeste del valle y de la ciudad de Cajamarca.El cerro de Santa Apolonia antes se le conocía con el nombre Rumi Tiana. La cultura Chavin es la que modifica la cima del cerro Santa Apolonia en el año 1,200 antes de Cristo.";
+}
+
